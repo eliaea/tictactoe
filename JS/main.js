d7d339172b9dd603b21d3740707d9770180266df
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-05-11 12:44:10 +0200

    style + manipulation updates

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,13 @@ box.addEventListener("click", () => {
 
     let random = Math.random() * 100 + 100;
 
-    let verticalMargin = Math.random() * 200 + 100;
-    let horizontalMargin = Math.random() * 200 + 100;
+    let left = Math.abs(Math.random() * 100 - 20);
+    let top = Math.abs(Math.random() * 100 - 20);
 
+    console.log(top)
     console.log(random)
     box.style.width = `${random}px`;
     box.style.height = `${random}px`;
-    box.style.margin = `${verticalMargin}px ${horizontalMargin}px`
+    box.style.left = `${left}%`
+    box.style.top = `${top}%`
 })
