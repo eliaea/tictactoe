e5d68e77cade53c90aea029b09f2cbdd2ace8222
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-06-08 13:02:46 +0200

    added edit message functionality

--- app/routes/messages.routes.js
+++ app/routes/messages.routes.js
@@ -5,5 +5,6 @@ const messages = require('../services/messages.service')
 
 router.get('/messages', messages.getMessages)
 router.post('/messages/add/message', messages.addMessage)
+router.put('/messages/edit/:name', messages.editMessage)
 
 module.exports = router
--- app/services/messages.service.js
+++ app/services/messages.service.js
@@ -17,7 +17,23 @@ const addMessage = async (req, res) => {
 
 }
 
+
+const editMessage = async (req, res) => {
+    const { name } = req.params
+    let body = req.body
+
+    console.log(name, body)
+    message = await messageModel.findOne({ name: name })
+
+    messageModel.where({ name: name }).update({ $set: { name: body.name } }).exec();
+
+    messages = await messageModel.find({})
+
+    return res.status(200).json({ message: "updated field", messages })
+}
+
 module.exports = {
     getMessages,
-    addMessage
+    addMessage,
+    editMessage
 }
