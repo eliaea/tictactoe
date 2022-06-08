cd9db6123cbd37facb23514f9966e69c7f37f758
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-06-08 12:38:15 +0200

    modified index.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 const app = express()
 const PORT = 4500
 
+try {
+    mongoose.connect('mongodb://root:example@localhost:27018', {})
+    console.log('Connected to database')
+} catch (error) {
+    console.log('Error connecting to db' + error)
+}
 
 // ROUTES
 const todosRouter = require('./routes/todos.routes')
 const testRouter = require('./routes/test.routes')
+const messageRouter = require('./routes/messages.routes')
 
 app.use(morgan('dev'))
 app.use(express.json())
@@ -15,6 +24,7 @@ app.use(express.json())
 // Adding routes
 app.use('/', todosRouter)
 app.use('/', testRouter)
+app.use('/', messageRouter)
 
 app.listen(PORT, () => {
     console.log('Server running on http://127.0.0.1:' + PORT)
